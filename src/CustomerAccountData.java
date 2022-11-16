0ca12dd76febec2621853637d96a527736068e76
Darshan Patil <iampatildarsh>
2022-11-16 21:30:35 +0530

    Commercial Data Processing Programme Added

---
+++ src/CustomerAccountData.java
@@ -0,0 +1,16 @@
+
+public interface CustomerAccountData {
+
+    public void stockAccount(String stock_name, int stock_nos, double stock_val);  // create a new account from file
+
+    public double valueOf();  // total value of account dollars
+
+    public void buy(int amount, double value, String symbol); // add shares of stock to account
+
+    public void sell(int amount, double value, String symbol); // subtract shares of stock from account
+
+    public void save(String filename);  //save account to file
+
+    public void printReport(); // print a detailed report of stocks and values
+}
+
